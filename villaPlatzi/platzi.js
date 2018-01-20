fb329309028e9170b3331d5f2501717327be3317
Fernando182 <ffhf.1820@gmail.com>
2018-01-20 08:54:38 -0300

    UPDATED platzi.js: moverLobo

--- villaPlatzi/platzi.js
+++ villaPlatzi/platzi.js
@@ -68,19 +68,17 @@ function dibujar()
 
   if(vaca.cargaOK)
   {
-    console.log("vaca "+vaca.cantidad);
     for(var v=0; v < vaca.cantidad; v++)
     {
-      var x = aleatorio(0, 7);
-      var y = aleatorio(0, 10);
-      var x = x * 60;
-      var y = y * 40;
+      var x = aleatorio(0, 5);
+      var y = aleatorio(0, 5);
+      var x = x * 80;
+      var y = y * 80;
       papel.drawImage(vaca.imagen, x, y);
     }
   }
   if(cerdo.cargaOK)
   {
-    console.log("Cerdo "+cerdo.cantidad);
     for(var c=0; c < cerdo.cantidad; c++)
     {
       var x = aleatorio(0, 4);
@@ -90,7 +88,6 @@ function dibujar()
       papel.drawImage(cerdo.imagen, x, y);
     }
   }
-
   if(pollo.cargaOK)
   {
     for(var p=0; p < pollo.cantidad; p++)
