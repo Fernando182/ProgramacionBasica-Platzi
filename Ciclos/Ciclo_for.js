966b691dee5ace071a7d65b7a801d169916a7aa5
Fernando182 <ffhf.1820@gmail.com>
2018-01-25 23:41:52 -0300

    ADDED Ciclo_for.js: loops test

--- Ciclos/Ciclo_for.js
+++ Ciclos/Ciclo_for.js
@@ -15,13 +15,25 @@ for (n in data) {
   tmpData[data[n]]+=1;
 }
 
-var ejemplo1 = [1,2,3,4,6,1,5];
-var ejemplo2 = [3,2,4,5,6,1,5];
+var a=0;
+var b=0;
 
-for (n in ejemplo1) {
-  if (ejemplo1[n] == ejemplo2[n]) {
-    document.write (ejemplo1[n]+" Igual "+ ejemplo2[n]+"<br />");
-  } else {
-    document.write (ejemplo1[n]+" Diff "+ ejemplo2[n]+ "<br />");
+var xEjemplo1 = [0,2,1,4,2,3];
+var yEjemplo1 = [0,3,2,3,2,1];
+
+var xEjemplo2 = [4,2,3];
+var yEjemplo2 = [3,2,1];
+
+for (var i = 0; i < xEjemplo1.length; i++) {
+  for (var j = 0; j < xEjemplo2.length; j++) {
+    if (xEjemplo1[i] == xEjemplo2[j] && yEjemplo1[i] == yEjemplo2[j]) {
+      document.write (xEjemplo1[i]+" y "+yEjemplo1[i]+" Igual a "+ xEjemplo2[j]+" y "+yEjemplo2[j]+"<br />");
+      a++;
+    } else {
+      document.write (xEjemplo1[i]+" y "+yEjemplo1[i]+" Diff a "+ xEjemplo2[j]+" y "+yEjemplo2[j]+"<br />");
+      b++;
+    }
   }
 }
+document.write (a +" Iguales <br />");
+document.write (b +" Diff <br />");
